156ad6470c7be8504b116c7d594749054d76c64a
Jinwon-Dev <yjw9424@naver.com>
2023-03-31 19:35:07 +0900

    스프링 데이터 JPA와 DB 설정, 동작 확인

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,23 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+    protected Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
